ef81c8dbb880b1ae1b72d569746b401b36115a76
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-24 10:06:27 -0400

    last

---
+++ src/LinkedListExample.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+
+        //Linked List Declaration
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        //add(String Element) is used for adding the elements to the linked list
+
+        linkedList.add("Item1");
+        linkedList.add("Item5");
+        linkedList.add("Item3");
+        linkedList.add("Item6");
+        linkedList.add("Item2");
+
+        //Display Linked list Content
+        System.out.println("Linked List Content: " +linkedList);
+
+        //Add First and Last Element
+        linkedList.addFirst("First Item");
+        linkedList.addLast("Last Item");
+        System.out.println("Linkedlist Content after addition: " +linkedList);
+
+        //This is how to get and set Values
+        Object firstvar = linkedList.get(0);
+        System.out.println("First element: " +firstvar);
+        linkedList.set(0, "Changed first item");
+        Object firstvar2 = linkedList.get(0);
+        System.out.println("First element after update by set method" +firstvar2);
+
+        //Remove first and las element
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        System.out.println("LinkedList after deletion of first and last element: " +linkedList);
+
+        //Add to a Position and remove from a position
+        linkedList.add(0, "Newly added item");
+        linkedList.remove(2);
+        System.out.println("Final Content: " +linkedList);
+    }
+}
